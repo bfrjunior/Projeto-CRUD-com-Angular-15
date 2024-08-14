618b6acb67e309895601fff6003922ebdffb5199
Fernando Rocha <beneditofernandor@gmail.com>
2024-08-14 16:47:15 -0300

    feat: Listagem de CLientes

--- src/app/principal/principal.component.ts
+++ src/app/principal/principal.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Cliente } from '../modelo/Cliente';
+import { ClienteService } from '../servico/cliente.service';
 
 @Component({
   selector: 'app-principal',
@@ -12,5 +14,17 @@ export class PrincipalComponent {
 
   // Variável para visibilidade da tabela
   tabela:boolean = true;
+
+   // JSON de clientes
+   clientes:Cliente[] = [];
+
+   // Construtor
+   constructor(private servico:ClienteService){}
+
+   // Método de seleção
+   selecionar():void{
+    this.servico.selecionar()
+    .subscribe(retorno => this.clientes = retorno);
+  }
 
 }
