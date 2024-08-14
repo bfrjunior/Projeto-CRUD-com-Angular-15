dbac74f5a122a4f18a06e37128c2c4bc1b8c0870
Fernando Rocha <beneditofernandor@gmail.com>
2024-08-14 16:42:14 -0300

    feat: Listagem de Clientes

--- src/app/servico/cliente.service.ts
+++ src/app/servico/cliente.service.ts
@@ -1,5 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Cliente } from '../modelo/Cliente';
 
 @Injectable({
   providedIn: 'root'
@@ -11,4 +13,9 @@ export class ClienteService {
 
   //Construtor
   constructor(private http:HttpClient) { }
+
+  //Método para selecionar todos os Clientes
+  selecionar():Observable<Cliente[]>{
+    return this.http.get<Cliente[]>(this.url);
+  }
 }
